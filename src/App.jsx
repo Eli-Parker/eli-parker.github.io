57e52f703e6918f65982a25ff51d1b6aad2e35fc
Eli Parker <racecar47@icloud.com>
2024-10-31 11:34:49 -0600

    Added a mobile warning screen

---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+import './style.css'
+import { Canvas } from '@react-three/fiber'
+import Experience from './Experience.jsx'
+import { Suspense, useState } from 'react'
+import LoadingScreen from './LoadingScreen.jsx'
+import { isMobile } from 'react-device-detect';
+
+/**
+ * 
+ * @returns A mobile warning if the user is on mobile, the experience otherwise
+ */
+export default function App() 
+{
+    const [continueTo3D, setContinueTo3D] = useState(false);
+
+    // Mobile experience
+    if (isMobile && !continueTo3D) {
+        return(
+        <>
+            <div className="mobile-screen" style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
+            <h3>Warning: This experience is <br/> not optimized for mobile devices,<br/> are you sure you want to continue? <br/><br/> Redirect will take you to<br/> a standard portfolio.</h3>
+            <div style={{ display: 'flex', gap: '10px', marginTop: '10px' }}>
+                <button className="button-1" role="button" onClick={() => setContinueTo3D(true)}>Continue</button>
+                <button className="button-1" role="button" onClick={() => window.location.href = 'https://eliparker.dev/react-site/'}>Redirect</button>
+            </div>
+            </div>
+        </>)
+    }
+
+    // Regular Desktop experience
+    else{
+        return <Canvas
+            className='r3f'
+            camera={ {
+                fov: 45,
+                near: 0.1,
+                far: 2000,
+                position: [ -3, 1.5, 6 ]
+            } }
+        >
+
+        {/* Trigger loading screen until loading finishes */}
+        <Suspense fallback={ <LoadingScreen /> }>
+            <Experience />
+        </Suspense>
+        </Canvas>
+    }
+}
